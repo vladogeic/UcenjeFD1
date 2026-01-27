9940aa2af1912c53939b56ea10ad94213177fc3f
vladogeic <vlado.geic@gmail.com>
2026-01-27 18:50:34 +0100

    Uvod u JavaScript

---
+++ 2_js/vjezba01/skripta.js
@@ -0,0 +1,13 @@
+console.info('Hello world S');
+console.table(JSON.parse('{"name":"John", "age":30, "car":null}'));
+console.log('O\'Neal') //\ATLGR+Q
+console.log("O' Neal");
+
+
+console.log(`
+    Hello
+    World
+    6 
+    `);// ' ALTGR + 7
+    
+    document.getElementById('ispis').innerHTML='<a href="#">Poveznica</a>';
