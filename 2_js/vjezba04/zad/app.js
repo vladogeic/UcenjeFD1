82018e09ac8aec2de5be8f80cf8c232106ad0a4d
vladogeic <vlado.geic@gmail.com>
2026-02-05 21:03:12 +0100

    5.2.26

--- 2_js/vjezba04/zad/app.js
+++ 2_js/vjezba04/zad/app.js
@@ -47,40 +47,111 @@ console.log(x)
 
 
   if(zadatak=== '2'){
-    const x = Number(a);
-    const y = Number(b);
-    const z=  Number(c);
 
-    if(!x && x!0){
-rezultat.innerHTML='Unesi broj u polje A'
-return;
-}
-
-if(!y && y!0){
-rezultat.innerHTML='Unesi broj u polje B'
-return;
-}
-
-if(!z && z!0){
-rezultat.innerHTML='Unesi broj u polje C'
-return;
-}
-
-
-if(x<y && x<z){!0
-  rezultat innerHTML=x
-}else if(y<x && y<z){
-  rezultat innerHTML =y
-}else{
-  rezultat innerHTML='Brojevi su jednaki'
-}
 
     return;
 
   }
   // Ovdje će doći drugi zadatak
 
-  
+  if(zadatak==='6'){
+
+const x = Number(a);
+const y = Number(b);
+const z = Number(c), k= Number(d);
+
+console.log(x,y,z,k)
+
+if(!x){
+  rezultat.innerHTML='Unesi broj u polje A';
+  return
+}
+if(!y){
+  rezultat.innerHTML='Unesi broj u polje B';
+  return
+}
+if(!z){
+  rezultat.innerHTML='Unesi broj u polje C';
+  return
+}
+if(!k){
+  rezultat.innerHTML='Unesi broj u polje D';
+  return
+}
+rezultat.innerHTML=(x+y)*(z+k);
+
+
+
+
+
+
+
+    return;
+
+  }
+// Za upisano ime grada u polje B ispiši je li sjedište Edunove ili ne</option>
+
+if(zadatak==='7'){
+
+  if(!b){
+
+    rezultat.innerHTML='Obavezan unaos u polje B';
+    return;
+  }
+
+    if(b.toLowerCase()==='osijek'){
+    rezultat.innerHTML=`${b} je sjedište Edunove`
+    }else{
+      rezultat.innerHTML=`${b} nije sjedište Edunove`
+    }
+    rezultat.innerHTML=b.tolowerCase()==='osijek';
+
+    
+    
+    
+    return;
+  }
+
+//Za upisanu vrijednost u polju A ispiši kubikažu
+
+  if(zadatak==='8'){
+    const x=Number(a)
+    if(!x || x<=0){
+
+      rezultat.innerHTML='Broj mora biti veći od 0'
+      return;
+    }
+    rezultat.innerHTML=x*x*x
+
+    return;
+
+
+  }  
+
+//Za upisanu vrijednost u polje D ispiši samo decimalni dio
+
+if (zadatak=== '9'){
+  const cijeliBroj =parseInt(d)
+  const decimalniBroj =parseFloat(d)
+  if(!decimalniBroj){
+rezultat.innerHTML= 'Niste unijeli broj'
+return;
+  }
+  console.log(cijeliBroj,decimalniBroj)
+  rezultat.innerHTML= decimalniBroj-cijeliBroj  
+
+return;
+}
+
+
+
+
+
+
+
+
+
+
   // ovo će se ispisati ako u HTML dodatke option za zadatak a ovdje ga ne obradite
   rezultat.innerHTML = `Nepoznati zadatak ${zadatak}`;
 });
---
+++ 2_js/vjezba5/app.js
@@ -0,0 +1,23 @@
+const ocjena= 4 ;
+
+switch(ocjena){
+case 1:
+    console.log('Nedovoljan');
+    break;
+    case 2:
+        console.log('Dovoljan');
+        break;
+        case 3:
+            console.log('Dobar');
+            break;
+            case 4:
+                console.log('Vrlo dobar');
+                break;
+                case 5 :
+                    console.log('Izvrstan');
+                    break;
+                    default :
+                    console.log('Nije ocjena');
+
+
+}
